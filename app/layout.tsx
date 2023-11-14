0a13287afe77e78a0d2a70f8bf863eea3aa588cf
Sanjib Poon <sanjibpoon123@gmail.com>
2023-11-14 14:16:13 +0545

    update homepage metadata and global styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Autodidact: Learn & Create',
-  description: 'From foundational to professional level, learn, create & collaborate and learn more...',
+  title: 'Autodidact: Learn, Create and Collaborate - i wanna learn',
+  description: 'Expand your knowledge and unleash your creativity with our comprehesive learning resources and collaborative platform. Start your journey today!',
 };
 
 export default function RootLayout({
