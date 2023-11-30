656469fd1ad4e46ecb53b37fc983f1e590f7dca0
Sanjib Poon <sanjibpoon123@gmail.com>
2023-11-30 19:46:24 +0545

    Style portfolio page

--- app/portfolio/components/projects-boulevard.tsx
+++ app/portfolio/components/projects-boulevard.tsx
@@ -1,21 +1,35 @@
 import Image from "next/image";
+import Logo from "@/app/images/logo.png";
 
 // Projects Boulevard Component
 export default function ProjectsBoulevard() {
     const projects = [
-        { name: 'Project A', description: 'Description of Project A', imageSrc: 'project-a.jpg' },
+        { name: 'PlayLearnForge', description: 'Your gamified learning wonderland', imageSrc: Logo },
         // Add more projects as needed
     ];
 
+    const projectClass = projects.length === 1 ? "col-span-full flex items-center justify-center" : "md:col-span-1 lg:col-span-1";
+
     return (
-        <div className="projects-boulevard">
-            {projects.map((project) => (
-                <div className="project-card" key={project.name}>
-                    <Image src={""} alt={project.name} />
-                    <h3>{project.name}</h3>
-                    <p>{project.description}</p>
-                </div>
-            ))}
+        <div className="p-4 mt-6">
+            <h2 className="text-2xl font-bold dark:text-gray-200 mb-4">Projects</h2>
+            <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ${projectClass}`}>
+                {projects.map((project) => (
+                    <div className={`rounded-lg overflow-hidden shadow-md dark:bg-gray-800 dark:text-white ${projectClass}`} key={project.name}>
+                        <div className="relative h-40">
+                            <Image
+                                src={project.imageSrc}
+                                alt={project.name}
+                                className="rounded-md w-full h-full p-2"
+                            />
+                        </div>
+                        <div className="p-4">
+                            <h3 className="text-xl font-semibold">{project.name}</h3>
+                            <p className="mt-2">{project.description}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
--- app/portfolio/components/skills-meadow.tsx
+++ app/portfolio/components/skills-meadow.tsx
@@ -5,13 +5,12 @@ export default function SkillsMeadow() {
         { skill: 'React.js', details: 'Built interactive user interfaces for web applications.' },
         { skill: 'Next.js', details: 'Developed fast and scalable React applications with server-side rendering.' },
         { skill: 'TailwindCSS', details: 'Styled web applications with a utility-first CSS framework.' },
-        { skill: 'shadcn-ui', details: 'Designed UI components for a seamless user experience.' },
         { skill: '@vercel/postgres', details: 'Managed databases for efficient data storage and retrieval.' },
     ];
 
     return (
-        <div className="skills-meadow p-4">
-            <h2 className="text-2xl font-bold text-gray-200 mb-4">Skills Meadow</h2>
+        <div className="p-4 mt-6">
+            <h2 className="text-2xl font-bold dark:text-gray-200 mb-4">Skills Meadow</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {skillsDetails.map((skill, index) => (
                     <Card key={index}>
