513693dd9720ef080288a63e35ed0b92d470842d
Sanjib Poon <sanjibpoon123@gmail.com>
2023-11-30 11:50:56 +0545

    Remove fetch protocol for now

--- app/quiz/mcqs/play/page.tsx
+++ app/quiz/mcqs/play/page.tsx
@@ -1,16 +1,16 @@
 import mcqs from '@/app/lib/data.json';
 import PlayMcq from './components/playMcq';
-import { fetchMcqs } from '@/app/lib/data';
+// import { fetchMcqs } from '@/app/lib/data';
 
-import { QuizData } from '@/app/lib/definitions';
+// import { QuizData } from '@/app/lib/definitions';
 
 export default async function Page() {
-    const quizzes = await fetchMcqs();
+    // const quizzes = await fetchMcqs();
     return (
         <div className='flex justify-center items-center pt-4'>
             <div className="bg-neutral-900 mx-2 my-8 p-6 shadow-lg rounded-lg overflow-auto w-full sm:w-9/12 md:w-9/12">
                 <h3 className="text-center my-2">JavaScript Quizzes</h3>
-                <PlayMcq mcqs={quizzes} />
+                <PlayMcq mcqs={mcqs} />
             </div>
         </div>
     );
