965b7b5ecdce25606d64af3ca0d4e8cfc093f76f
Sanjib Poon <sanjibpoon123@gmail.com>
2023-12-01 08:46:24 +0545

    Remove bg color for light mode

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,6 +1,6 @@
 export default function Card({ children }: { children: React.ReactNode }) {
     return (
-        <div className="bg-white dark:bg-gray-800 p-4 rounded-md shadow-lg">
+        <div className="dark:bg-gray-800 p-4 rounded-md shadow-lg">
             {children}
         </div>
     );
