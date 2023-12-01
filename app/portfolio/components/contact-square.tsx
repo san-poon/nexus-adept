f5dd99e07eb7e000f38cd04069ce2137b0410797
Sanjib Poon <sanjibpoon123@gmail.com>
2023-12-01 09:37:49 +0545

    Style contacts section

--- app/portfolio/components/contact-square.tsx
+++ app/portfolio/components/contact-square.tsx
@@ -1,11 +1,24 @@
-// Contact Square Component
+// Portfolio contacts
 export default function ContactSquare() {
+    const contactInfo = [
+        { label: 'Email', value: 'sanjibpoon123@gmail.com' },
+        { label: 'GitHub', value: 'github.com/san-poon' },
+        { label: 'LinkedIn', value: 'linkedin.com/in/san-poon' },
+        { label: 'Twitter', value: 'twitter.com/san_poon2' },
+    ];
+
     return (
-        <div className="contact-square">
-            <h2>Contact Square</h2>
-            <p>Email: your.email@example.com</p>
-            <p>LinkedIn: linkedin.com/in/yourprofile</p>
-            <p>GitHub: github.com/yourusername</p>
+        <div className="mt-6 p-2">
+            <h2 className="text-2xl font-semibold mb-4">Contact Square</h2>
+            <div className="bg-gradient-to-r dark:from-gray-800 dark:to-gray-600 from-gray-300 to-gray-500 p-8 rounded-md shadow-lg max-w-2xl mx-auto">
+                <ul className="list-disc ml-6">
+                    {contactInfo.map(({ label, value }) => (
+                        <li key={label} className="mb-2">
+                            <span className="font-bold">{label}:</span> {value}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
--- app/portfolio/components/projects-boulevard.tsx
+++ app/portfolio/components/projects-boulevard.tsx
@@ -1,30 +1,35 @@
 import Image from "next/image";
 import Logo from "@/app/images/logo.png";
+import Link from "next/link";
 
 // Projects Boulevard Component
 export default function ProjectsBoulevard() {
     const projects = [
-        { name: 'PlayLearnForge', description: 'Your gamified learning wonderland', imageSrc: Logo },
+        { name: 'PlayLearnForge', description: 'Your gamified learning wonderland', imageSrc: Logo, projectUrl: "/" },
         // Add more projects as needed
     ];
 
     const projectClass = projects.length === 1 ? "col-span-full flex items-center justify-center" : "md:col-span-1 lg:col-span-1";
 
     return (
-        <div className="p-4 mt-6">
+        <div className="p-2 mt-6">
             <h2 className="text-2xl font-bold dark:text-gray-200 mb-4">Projects</h2>
             <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ${projectClass}`}>
                 {projects.map((project) => (
                     <div className={`rounded-lg overflow-hidden shadow-md dark:bg-gray-800 dark:text-white ${projectClass}`} key={project.name}>
                         <div className="relative h-40">
-                            <Image
-                                src={project.imageSrc}
-                                alt={project.name}
-                                className="rounded-md w-full h-full p-2"
-                            />
+                            <Link href={project.projectUrl}>
+                                <Image
+                                    src={project.imageSrc}
+                                    alt={project.name}
+                                    className="rounded-md w-auto h-full p-2 transition-transform transform hover:scale-105"
+                                />
+                            </Link>
                         </div>
                         <div className="p-4">
-                            <h3 className="text-xl font-semibold">{project.name}</h3>
+                            <Link href={project.projectUrl}>
+                                <h3 className="text-xl font-semibold">{project.name}</h3>
+                            </Link>
                             <p className="mt-2">{project.description}</p>
                         </div>
                     </div>
