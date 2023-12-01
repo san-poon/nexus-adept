e6504a49700db7d60a84ead4300150e58818c29b
Sanjib Poon <sanjibpoon123@gmail.com>
2023-12-01 09:41:32 +0545

    Remove max width from portfolio contacts container

--- app/portfolio/components/contact-square.tsx
+++ app/portfolio/components/contact-square.tsx
@@ -10,7 +10,7 @@ export default function ContactSquare() {
     return (
         <div className="mt-6 p-2">
             <h2 className="text-2xl font-semibold mb-4">Contact Square</h2>
-            <div className="bg-gradient-to-r dark:from-gray-800 dark:to-gray-600 from-gray-300 to-gray-500 p-8 rounded-md shadow-lg max-w-2xl mx-auto">
+            <div className="bg-gradient-to-r dark:from-gray-800 dark:to-gray-600 from-gray-300 to-gray-500 p-8 rounded-md shadow-lg">
                 <ul className="list-disc ml-6">
                     {contactInfo.map(({ label, value }) => (
                         <li key={label} className="mb-2">
