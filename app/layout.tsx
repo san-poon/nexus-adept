7008c8d4b5eacdfbf7171c451df522f04137f270
Sanjib Poon <sanjibpoon123@gmail.com>
2023-12-02 10:56:40 +0545

    change app title metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from './components/theme-provider';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Autodidact: Learn, Create and Collaborate - i wanna learn',
+  title: 'Autodidact: Play, Learn, Forge',
   description: 'Expand your knowledge and unleash your creativity with our comprehesive learning resources and collaborative platform. Start your journey today!',
 };
 
