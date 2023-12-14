50908635bf479d6493863a09ec70168d0cc6f0c6
Sanjib Poon <sanjibpoon123@gmail.com>
2023-12-14 15:53:21 +0545

    Remove state & effect hooks

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,5 @@
 'use client';
 import Link from "next/link";
-import { useEffect, useState } from "react";
 import ModeToggle from "./theme-toggler";
 import Logo from "./logo";
 import { Button } from "./Button";
