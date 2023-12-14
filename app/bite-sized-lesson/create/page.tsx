29fa5a3ff2817e5b90a4bd0cd794e4b58573c5e3
Sanjib Poon <sanjibpoon123@gmail.com>
2023-12-14 17:41:31 +0545

    Add logic to show button name except on small screens

--- app/bite-sized-lesson/create/page.tsx
+++ app/bite-sized-lesson/create/page.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 
 const CreatePage = () => {
     const [isFixed, setIsFixed] = useState(false);
-    const headerHeightPercentage = 29;
+    const headerHeightPercentage = 28;
 
     useEffect(() => {
         const handleScroll = () => {
@@ -40,7 +40,7 @@ const CreatePage = () => {
                 </div>
 
                 {/* Right Side - Live Preview */}
-                <div className={`flex-shrink-0 w-full md:w-2/3 px-4 overflow-y-auto ${isFixed ? '' : ''}`}>
+                <div className={`flex-shrink-0 w-full md:w-2/3 px-4 overflow-y-auto`}>
                     {/* Live Preview Goes Here */}
                     <div className="bg-white dark:bg-gray-700 p-4 rounded shadow h-full">
                         <LessonPreview />
@@ -96,7 +96,8 @@ const TextModalButton = () => {
                         d="M4 6h16M4 12h16m-7 6h7"
                     />
                 </svg>
-
+                {/* show button name, except on small screens */}
+                <span className='hidden sm:inline md:inline ms-1'>Text</span>
             </Button>
         </>
     )
@@ -110,7 +111,8 @@ const ImageModalButton = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                 </svg>
-
+                {/* show button name, except on small screens */}
+                <span className='hidden sm:inline md:inline ms-2'>Image</span>
             </Button>
         </>
     )
@@ -124,8 +126,8 @@ const QuizModalButton = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" className='w-6 h-6'>
                     <path strokeLinecap="round" strokeLinejoin="round" stroke="currentColor" fill="currentColor" d="M560-360q17 0 29.5-12.5T602-402q0-17-12.5-29.5T560-444q-17 0-29.5 12.5T518-402q0 17 12.5 29.5T560-360Zm-30-128h60q0-29 6-42.5t28-35.5q30-30 40-48.5t10-43.5q0-45-31.5-73.5T560-760q-41 0-71.5 23T446-676l54 22q9-25 24.5-37.5T560-704q24 0 39 13.5t15 36.5q0 14-8 26.5T578-596q-33 29-40.5 45.5T530-488ZM320-240q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320Zm0-80h480v-480H320v480ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-720v480-480Z" />
                 </svg>
-
-
+                {/* show button name, except on small screens */}
+                <span className='hidden sm:inline md:inline ms-2'>Quiz</span>
             </Button>
         </>
     )
@@ -140,8 +142,8 @@ const CodeSandboxModalButton = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
                 </svg>
-
-
+                {/* show button name, except on small screens */}
+                <span className='hidden sm:inline md:inline ms-2'>Code</span>
             </Button>
         </>
     )
