f900b33316e94cc4dd1f64cebfec73ca2ce8fdc2
Sanjib Poon <sanjibpoon123@gmail.com>
2023-12-16 07:05:33 +0545

    Add event handlers and content modal for text & image type

---
+++ app/bite-sized-lesson/create/content-modal.tsx
@@ -0,0 +1,73 @@
+import { Button } from "@/app/components/Button";
+import Image from "next/image";
+
+const ContentModal = ({ onClose, children }: any) => {
+    return (
+        <>
+            <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-75 flex">
+                <div className="relative p-4 mx-auto my-8 w-11/12 lg:w-8/12 bg-white dark:bg-neutral-900 rounded-md">
+                    {/* Header */}
+                    <div className="flex justify-between items-center border-b pb-2 mb-4">
+                        <h2 className="text-xl font-semibold">Create a Lesson</h2>
+                        <Button onClick={onClose}>
+                            x
+                        </Button>
+                    </div>
+
+                    {/* Content */}
+                    <div>
+                        {children}
+                    </div>
+
+                    {/* Footer */}
+                    <div className="absolute bottom-4 right-4">
+                        <Button>Save</Button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export const TextModal = ({ onClose, onTextareaChange }: any) => {
+    return (
+        <>
+            <ContentModal onClose={onClose}>
+                <textarea onChange={onTextareaChange}>This is a textarea that is going to be lit</textarea>
+            </ContentModal>
+        </>
+    )
+}
+
+export const ImageModal = ({ onClose, onImageUpload, selectedImage }: any) => {
+
+    return (
+        <ContentModal onClose={onClose}>
+            {/* Image Upload Form */}
+            <div className="flex flex-col items-center space-y-4">
+                {/* Image Preview */}
+                {selectedImage && (
+                    <Image src={selectedImage} alt="Preview" className="max-w-full max-h-64 object-contain" width={500} height={300} />
+                )}
+
+                {/* Image Input */}
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={onImageUpload}
+                    className="border p-2"
+                />
+
+                {/* Image Size Recommendations */}
+                <p className="text-sm text-gray-500">Recommended sizes: Thumbnail, Medium, Large</p>
+
+                {/* Upload Button */}
+                <button className="bg-blue-500 text-white py-2 px-4 rounded">
+                    Upload
+                </button>
+            </div>
+        </ContentModal>
+    )
+}
+
+export default ContentModal;
--- app/bite-sized-lesson/create/page.tsx
+++ app/bite-sized-lesson/create/page.tsx
@@ -1,20 +1,57 @@
-
+'use client';
 import { Button } from '@/app/components/Button';
+import { useState } from 'react';
+import ContentModal, { ImageModal, TextModal } from './content-modal';
 
 const CreatePage = () => {
+    const [lessonTitle, setLessonTitle] = useState('');
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [text, setText] = useState('');
+    const [selectedImage, setSelectedImage] = useState<any>(null);
+
+    const handleImageChange = (event: any) => {
+        const file = event.target.files[0];
+
+        // Check if the selected file is an image
+        if (file && file.type.startsWith('image/')) {
+            // Display a preview of the image
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setSelectedImage(reader.result);
+            };
+            reader.readAsDataURL(file);
+        } else {
+            console.warn('Please choose a valid image file.');
+        }
+    }
+
+
+    const openModal = () => setModalOpen(true);
+    const closeModal = () => setModalOpen(false);
+
+    const handleTitleChange = (event: any) => {
+        event.preventDefault();
+        setLessonTitle(event.target.value);
+    }
+
+    const handleTextareaChange = (event: any) => {
+        event.preventDefault();
+        setText(event.target.value);
+    }
+
     return (
         <>
             <div className="md:w-2/3 mx-auto p-4">
                 {/* Title Input */}
-                <TitleInput />
+                <TitleInput title={lessonTitle} onTitleChange={handleTitleChange} />
             </div>
             <div className={`flex flex-col md:flex-row h-screen`}>
                 {/* Left Side - Content Buttons */}
                 <div className={` flex justify-center items-center w-full md:w-1/3 md:h-3/4 p-4 bg-neutral-100 dark:bg-neutral-800 rounded shadow transition sticky top-0 md:top-4`}>
                     {/* Content Buttons */}
                     <div className="flex md:flex-col  spacek-y-2 md:space-y-6 space-x-2 justify-center items-baseline">
-                        <TextModalButton />
-                        <ImageModalButton />
+                        <TextModalButton isOpen={isModalOpen} openModal={openModal} onClose={closeModal} onTextareaChange={handleTextareaChange} />
+                        <ImageModalButton isOpen={isModalOpen} openModal={openModal} onClose={closeModal} onImageUpload={handleImageChange} selectedImage={selectedImage} />
                         <QuizModalButton />
                         <CodeSandboxModalButton />
                     </div>
@@ -24,7 +61,7 @@ const CreatePage = () => {
                 <div className={`flex-shrink-0 w-full md:w-2/3 px-4 overflow-y-auto`}>
                     {/* Live Preview Goes Here */}
                     <div className="bg-white dark:bg-gray-700 p-4 rounded shadow h-full">
-                        <LessonPreview />
+                        <LessonPreview title={lessonTitle} text={text} />
                     </div>
                 </div>
             </div>
@@ -33,35 +70,37 @@ const CreatePage = () => {
 };
 
 
-const LessonPreview = () => {
+const LessonPreview = ({ title, text }: { title: string, text: string }) => {
     return (
         <>
-            {/* Live Preview Content */}
-            <p className="text-lg font-bold mb-2">Live Preview</p>
-            {/* Add your live preview content here */}
+            <h1 className="text-4xl">{title}</h1>
+            <p>{text}</p>
         </>
     )
 }
 
-const TitleInput = () => {
+const TitleInput = ({ title, onTitleChange }: any) => {
     return (
         <>
             {/* Lesson Title input */}
             <input
                 type="text"
                 placeholder="Lesson Title"
+                value={title}
                 className="w-full p-2 mb-4 border-b-2 border-blue-500 outline-none rounded shadow"
+
+                onChange={onTitleChange}
             />
         </>
     );
 }
 
 
-const TextModalButton = () => {
+const TextModalButton = ({ isOpen, openModal, onClose, onTextareaChange }: any) => {
     return (
         <>
             {/* Text Button */}
-            <Button className="">
+            <Button className="" onClick={openModal}>
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -80,21 +119,24 @@ const TextModalButton = () => {
                 {/* show button name, except on small screens */}
                 <span className='hidden sm:inline md:inline ms-1'>Text</span>
             </Button>
+
+            {isOpen && <TextModal onClose={onClose} onTextareaChange={onTextareaChange} />}
         </>
     )
 }
 
-const ImageModalButton = () => {
+const ImageModalButton = ({ isOpen, openModal, onClose, onImageUpload, selectedImage }: any) => {
     return (
         <>
             {/* Images Button */}
-            <Button className="">
+            <Button className="" onClick={openModal}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                 </svg>
                 {/* show button name, except on small screens */}
                 <span className='hidden sm:inline md:inline ms-2'>Image</span>
             </Button>
+            {isOpen && <ImageModal onClose={onClose} onImageUpload={onImageUpload} selectedImage={selectedImage} />}
         </>
     )
 }
