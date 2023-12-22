638e47bc54bcd4d374d8c6548714c4029be255c3
Sanjib Poon <sanjibpoon123@gmail.com>
2023-12-22 15:05:46 +0545

    Add missing imports in cofig

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const withMDX = require('@next/mdx')()
+
 const nextConfig = {
     // Configure `pageExtensions` to include MDX files
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
     // Optionally, add any other Next.js config below
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
