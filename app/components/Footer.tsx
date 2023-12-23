5f73dd298985f4a098f275f4d37ec044bd04ff2e
Sanjib Poon <sanjibpoon123@gmail.com>
2023-12-23 14:00:32 +0545

    Refact footer for cleaner UI

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,42 +4,46 @@ export default function Footer() {
     return (
         <footer className="p-4">
             <div className="container mx-auto text-center dark:bg-neutral-800 rounded-md p-4 z-50">
-                <p className="text-sm">
-                    &copy; 2023 Autodidact Co. All rights reserved.
+                <p className="text-xs">
+                    &copy; 2023 PlayLearnForge.
                 </p>
-                <p className="text-xs mb-2">Play, Learn, Create</p>
+                {/* <p className="text-xs mb-2">Play, Learn, Create</p> */}
                 {/* Contact Information */}
-                <div className="flex justify-center space-x-4">
-                    <a href="#" className="hover:underline">
-                        Email Us
-                    </a>
-                    <a
-                        href="twitter.com/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="hover:underline"
-                    >
-                        Twitter
-                    </a>
+                <div className="flex justify-center space-x-8 mt-4">
+                    <div className="hover:dark:bg-neutral-700 hover:bg-neutral-200 px-1 rounded">
+                        <a href="mailto:sanjibpoon123@gmail.com" className="">
+                            Email
+                        </a>
+                    </div>
+                    <div className="hover:dark:bg-neutral-700 hover:bg-neutral-200 px-1 rounded">
+                        <a
+                            href="https://twitter.com/san_poon_2"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className=""
+                        >
+                            Twitter
+                        </a>
+                    </div>
                 </div>
 
                 {/* Quick Links */}
-                <div className="mt-4 shadow-md">
+                {/* <div className="mt-4 shadow-md">
                     <Link href="/" className="mr-4 hover:text-blue-500 transition duration-300 ease-in-ou">Home</Link>
                     <Link href="#" className="mr-4 hover:text-green-500 transition duration-300 ease-in-ou">Play</Link>
                     <Link href="#" className="mr-4  hover:text-pink-500 transition duration-300 ease-in-ou">Contribute</Link>
-                </div>
+                </div> */}
 
                 {/* Legal Information */}
-                <div className="mt-4">
+                {/* <div className="mt-4">
                     <Link href="#" className="mr-4 hover:underline">Terms of Use</Link>
                     <Link href="#" className="hover:underline">Privacy Policy</Link>
-                </div>
+                </div> */}
 
                 {/* Acknowledgments */}
                 <div className="mt-4">
                     <p className="text-xs">
-                        Proudly built by Autodidact community. Thanks to our contributors!
+                        Proudly created by Sanjib(sanjibpoon123@gmail.com), the Autodidact.
                     </p>
                 </div>
             </div>
