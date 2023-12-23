06d8f97ba6cab03c1ab37b831a5c70bd13f21895
Sanjib Poon <sanjibpoon123@gmail.com>
2023-12-23 20:22:53 +0545

    Some minor css changes

--- app/components/hero-section.tsx
+++ app/components/hero-section.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/app/components/Button";
 
 const HeroComponent = () => {
     return (
-        <section className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-white py-16">
+        <section className="bg-gradient-to-r from-neutral-500 via-neutral-600 to-neutral-700 dark:from-neutral-900 dark:via-neutral-800 dark:to-neutral-700 text-white py-16">
             <div className="container mx-auto text-center">
                 {/* Logo Image */}
                 <Image src={logo} alt="Play, Learn & Forge logo" className="mx-auto w-16 h-16 mb-4 rounded-full" />
@@ -17,17 +17,17 @@ const HeroComponent = () => {
                 </p>
                 <div className="flex flex-col items-center mb-4">
                     <Link href="/quiz/mcqs/create" title="Create quizz">
-                        <Button className="bg-white dark:bg-neutral-800 text-blue-500 px-6 py-3 rounded-full font-bold text-lg hover:bg-blue-500 hover:text-white transition duration-300">
+                        <Button className="hover:text-black dark:hover:text-white dark:hover:bg-neutral-700">
                             Create Quiz
                         </Button>
                     </Link>
                     <Link href="/bite-sized-lesson/create" title="Create course lesson and get paid">
-                        <Button className=" mt-4 bg-white dark:bg-neutral-800 text-blue-500 px-6 py-3 rounded-full font-bold text-lg hover:bg-blue-500 hover:text-white transition duration-300">
+                        <Button className=" mt-4 hover:text-black dark:hover:text-white dark:hover:bg-neutral-700">
                             Create Lesson
                         </Button>
                     </Link>
                     <Link href="/quiz/mcqs/play" title="Play Javascript quizzes">
-                        <Button className="mt-4 bg-white dark:bg-neutral-800 text-blue-500 px-6 py-3 rounded-full font-bold text-lg hover:bg-blue-500 hover:text-white transition duration-300">Play</Button>
+                        <Button className="mt-4 hover:text-black dark:hover:text-white dark:hover:bg-neutral-700">Play</Button>
                     </Link>
                 </div>
             </div>
