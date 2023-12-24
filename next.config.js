e43c8c15c84d8643e0d24279b0965f7b2fd5cdaf
Sanjib Poon <sanjibpoon123@gmail.com>
2023-12-24 10:43:27 +0545

    Remove mdx support

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const withMDX = require('@next/mdx')()
 
 const nextConfig = {
     // Configure `pageExtensions` to include MDX files
-    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
     // Optionally, add any other Next.js config below
 }
 
-module.exports = withMDX(nextConfig)
+module.exports = nextConfig
