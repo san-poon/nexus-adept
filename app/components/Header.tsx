16e52b9ea417c345867db9fea3a4d79d89f6853d
Sanjib Poon <sanjibpoon123@gmail.com>
2023-12-25 11:43:54 +0545

    Refactor position and height of the menu items

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -41,12 +41,10 @@ export default function Header() {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                     {isMenuOpen && (
-                        <div className={`flex flex-col border-2 border-neutral-300 dark:border-neutral-800 bg-neutral-50 dark:bg-neutral-950 rounded-lg p-2 px-4 absolute end-0 -bottom-24 z-10 transition-opacity duration-300
-                            ${isMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}
-                        `}>
-                            <Link href="/bite-sized-lesson/create" className="">Create</Link>
-                            <Link href="/quiz/mcqs/play" className="">Play</Link>
-                            <Link href="/portfolio" className="">Portfolio</Link>
+                        <div className="flex flex-col border-2 border-neutral-300 dark:border-neutral-800 bg-neutral-50 dark:bg-neutral-950 rounded-lg p-2 px-4 absolute end-0 -bottom-28 z-10 justify-center space-y-2">
+                            <Link href="/bite-sized-lesson/create" className="active:bg-neutral-500 active:dark:bg-neutral-500">Create</Link>
+                            <Link href="/quiz/mcqs/play" className="active:bg-neutral-500 active:dark:bg-neutral-500">Play</Link>
+                            <Link href="/portfolio" className="active:bg-neutral-500 active:dark:bg-neutral-500">Portfolio</Link>
                         </div>
                     )}
                 </div>
