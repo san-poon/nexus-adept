a233f6bf13f9e8f5ce863c1e3c006ae906bbf0fb
Sanjib Poon <sanjibpoon123@gmail.com>
2023-12-27 11:19:59 +0545

    Some minor changes

---
+++ app/bite-sized-lesson/create/components/text-combobox.tsx
@@ -0,0 +1,18 @@
+
+import { Check, ChevronsUpDown } from "lucide-react";
+import clsx from "clsx";
+import { Button } from "@/app/components/Button";
+
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from "@/components/ui/command";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+
+const textContentTypes = [
+    {
+        value: 'sub-heading',
+        label: 'Sub-Heading',
+    },
+    {
+        value: ''
+    }
+]
+
