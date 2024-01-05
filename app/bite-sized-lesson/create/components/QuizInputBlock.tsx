28ce8a01155a4dc04a85780a77f181e4ee5e656d
Sanjib Poon <sanjibpoon123@gmail.com>
2024-01-05 13:42:36 +0545

    Remove form as a whole, Didn't like the complexity

--- app/bite-sized-lesson/create/components/QuizInputBlock.tsx
+++ app/bite-sized-lesson/create/components/QuizInputBlock.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
@@ -16,16 +17,18 @@ import { DynamicTextarea } from "./content-blocks";
 import { quizSchema } from '@/app/lib/schemas/quizSchema';
 import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
-import { Input } from '@/components/ui/input';
 
+// When Quiz Input Button is clicked, it will create
+// a quiz data structure, and QuizInputBlock will render the
+// data with initial quiz contents.
 const initialQuizContent = {
     id: uuidv4(),
     question: 'What is something divided by zero?',
     options: [
-        { value: 'Infinity', isCorrect: false },
-        { value: 'Zero', isCorrect: false },
-        { value: 'undefined', isCorrect: true },
-        { value: 'Error', isCorrect: false },
+        { id: uuidv4(), value: 'Infinity', isCorrect: false },
+        { id: uuidv4(), value: 'Zero', isCorrect: false },
+        { id: uuidv4(), value: 'undefined', isCorrect: true },
+        { id: uuidv4(), value: 'Error', isCorrect: false },
     ],
     explanation: `
     In mathematics, dividing anything by zero is simply undefined. 
@@ -38,93 +41,47 @@ const initialQuizContent = {
 };
 
 export default function QuizInputBlock() {
-    const form = useForm<z.infer<typeof quizSchema>>({
-        resolver: zodResolver(quizSchema),
-    });
+    const [quiz, setQuiz] = useState(initialQuizContent)
 
-    const onSubmit = (data: z.infer<typeof quizSchema>) => {
-        console.log(data);
-    };
+    const handleCheckboxChange = (optionId: String, isChecked: boolean) => {
+        const nextOptions = [...quiz.options].map((option) => {
+            if (option.id === optionId) {
+                return { ...option, isCorrect: isChecked }
+            } else {
+                return option
+            }
+        })
+        setQuiz({ ...quiz, options: nextOptions })
 
+    }
     return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
-                <FormField
-                    control={form.control}
-                    name="question"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Question</FormLabel>
-                            <FormControl>
-                                <DynamicTextarea
-                                    className="px-2 py-2 my-2"
-                                    rows={1}
-                                    placeholder={initialQuizContent.question}
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
+        <div>
+            <div>
+                <label>Question</label>
+                <DynamicTextarea
+                    rows={1}
+                    className="px-2 py-2 my-2"
+                    placeholder={quiz.question}
                 />
-                <div>
-                    {initialQuizContent['options'].map((option, index) => (
-                        <FormField
-                            key={index}
-                            control={form.control}
-                            name="options"
-                            render={({ field }) => (
-                                <div key={index}>
-                                    <div className='flex m-3 items-center'>
-                                        <FormItem>
-                                            <FormControl>
-                                                <Checkbox
-                                                    checked={field.value?.includes(option)}
-                                                    onChange={(checked) => {
-                                                        return checked
-                                                            ? field.onChange([...field.value, option])
-                                                            : field.onChange(
-                                                                field.value?.filter((value) => value !== option)
-                                                            )
-                                                    }}
-                                                />
-                                            </FormControl>
-                                        </FormItem>
-                                        <FormItem>
-                                            <FormControl>
-                                                <DynamicTextarea
-                                                    rows={1}
-                                                    className="px-2 mx-2 py-2"
-                                                    placeholder={`Option ${index + 1}`}
-                                                    {...field.value}
-                                                />
-                                            </FormControl>
-                                        </FormItem>
-                                    </div>
-                                </div>
-                            )}
+            </div>
+            <div>
+                <label>Options</label>
+                {quiz.options.map((option, index) => (
+                    <div key={index} className='flex items-center space-x-2 m-2'>
+                        <input
+                            className='h-4 w-4'
+                            type="checkbox"
+                            checked={option.isCorrect}
+                            onChange={(e) => handleCheckboxChange(option.id, e.target.checked)}
                         />
-                    ))}
-                </div>
-                <FormField
-                    control={form.control}
-                    name="explanation"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Explanation</FormLabel>
-                            <FormControl>
-                                <DynamicTextarea
-                                    rows={1}
-                                    className='px-2 py-2 my-2'
-                                    placeholder="Explanation here..."
-                                    {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit">Submit</Button>
-            </form>
-        </Form >
+                        <DynamicTextarea
+                            rows={1}
+                            className='px-2 py-2 my-2'
+                            placeholder={`Option: ${index + 1}`}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
