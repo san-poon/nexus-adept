e7b99ba839e192840c231dd68a193a4336d6a084
Sanjib Poon <sanjibpoon123@gmail.com>
2024-01-11 13:23:23 +0545

    negligible changes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,3 @@
-'use client';
 import Link from "next/link";
 import ThemeToggler from "./theme-toggler";
 import Logo from "./logo";
--- app/components/theme-toggler.tsx
+++ app/components/theme-toggler.tsx
@@ -11,7 +11,7 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
-export default function ModeToggle() {
+export default function ThemeToggler() {
     const { setTheme } = useTheme()
 
     return (
