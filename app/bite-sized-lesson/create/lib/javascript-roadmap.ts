58d2729e8eca63030fe1f536da238bc72de2370e
Sanjib Poon <sanjibpoon123@gmail.com>
2024-01-12 11:52:24 +0545

    Add basci javascript subject structure

---
+++ app/bite-sized-lesson/create/lib/javascript-roadmap.ts
@@ -0,0 +1,44 @@
+const course = {
+    name: "JavaScript",
+    chapters: [
+        {
+            name: "Introduction to JavaScript",
+            lessons: [
+                { name: "What is JavaScript", lessonContent: [] },
+                { name: "History of JavaScript", lessonContent: [] },
+                { name: "JavaScript Versions", lessonContent: [] },
+                { name: "How to run JavaScript", lessonContent: [] }
+            ]
+        },
+        {
+            name: "All about variables",
+            lessons: [
+                { name: "Variable Declarations", lessonContent: [] },
+                { name: "Hoisting", lessonContent: [] },
+                { name: "Variable Naming Rules", lessonContent: [] },
+                { name: "Variable Scopes", lessonContent: [] }
+            ]
+        },
+        {
+            name: "Data Types",
+            lessons: [
+                { name: "Primitive Types", lessonContent: [] }
+            ]
+        },
+        {
+            name: "Type Casting",
+            lessons: [
+                { name: "Explicit & Implicit Type Casting", lessonContent: [] },
+                { name: "Type Conversion vs Coercion", lessonContent: [] }
+            ]
+        },
+        {
+            name: "Data Structures",
+            lessons: [
+                { name: "Structured Data: JSON", lessonContent: [] },
+                { name: "Keyed Collections", lessonContent: [] },
+                { name: "Indexed Collections", lessonContent: [] },
+            ],
+        },
+    ]
+};
