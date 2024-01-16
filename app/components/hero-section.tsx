67c88842107797b276c692536b1dc24e0eecd84d
Sanjib Poon <sanjibpoon123@gmail.com>
2024-01-16 18:31:10 +0545

    Add proper links to links of features

--- app/components/hero-section.tsx
+++ app/components/hero-section.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/app/components/Button";
 
 const HeroComponent = () => {
     return (
-        <section className="bg-gradient-to-r from-neutral-500 via-neutral-600 to-neutral-700 dark:from-neutral-900 dark:via-neutral-800 dark:to-neutral-700 text-white py-16">
+        <section className="bg-gradient-to-r from-neutral-500 via-neutral-600 to-neutral-700 dark:from-neutral-900 dark:via-neutral-800 dark:to-neutral-700 text-white py-16 h-screen">
             <div className="container mx-auto text-center">
                 {/* Logo Image */}
                 <Image src={logo} alt="Play, Learn & Forge logo" className="mx-auto w-16 h-16 mb-4 rounded-full" />
@@ -16,17 +16,17 @@ const HeroComponent = () => {
                     Welcome to the vibrant town square of learning and creation.
                 </p>
                 <div className="flex flex-col items-center mb-4">
-                    <Link href="/quiz/mcqs/create" title="Create quizz">
+                    <Link href="/feature-quiz/mcqs/create" title="Create quizz">
                         <Button className="hover:text-black dark:hover:text-white dark:hover:bg-neutral-700">
                             Create Quiz
                         </Button>
                     </Link>
-                    <Link href="/bite-sized-lesson/create" title="Create course lesson and get paid">
+                    <Link href="/feature-lesson/create" title="Create course lesson and get paid">
                         <Button className=" mt-4 hover:text-black dark:hover:text-white dark:hover:bg-neutral-700">
                             Create Lesson
                         </Button>
                     </Link>
-                    <Link href="/quiz/mcqs/play" title="Play Javascript quizzes">
+                    <Link href="/feature-quiz/mcqs/play" title="Play Javascript quizzes">
                         <Button className="mt-4 hover:text-black dark:hover:text-white dark:hover:bg-neutral-700">Play</Button>
                     </Link>
                 </div>
