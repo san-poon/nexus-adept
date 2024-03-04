965f937c1c07113e2b51f11d046685001e30d386
Sanjib Poon <sanjibpoon123@gmail.com>
2024-03-04 18:27:59 +0545

    Remove rubik from global import

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} ${rubik.className}  flex flex-col min-h-screen dark:text-neutral-100`}>
+      <body className={` ${inter.className}  flex flex-col min-h-screen dark:text-neutral-100`}>
         <ThemeProvider attribute='class'
           defaultTheme='system'
           enableSystem
