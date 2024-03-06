9a12b7efda9a79b1541074c194b6c7ae784572e3
Sanjib Poon <sanjibpoon123@gmail.com>
2024-03-06 15:52:14 +0545

    Replace font-medium to font-normal for button, and remove unneccessary hover text color effect

--- app/components/hero-section.tsx
+++ app/components/hero-section.tsx
@@ -5,11 +5,11 @@ import { Button } from "@/components/ui/button";
 
 const HeroComponent = () => {
     return (
-        <section className="relative dark:text-white py-16 h-screen">
+        <section className="relative py-16 h-screen">
             <div className="container mx-auto text-center">
                 {/* Logo Image */}
                 <Image src={logo} alt="Play, Learn & Forge logo" className="mx-auto w-16 h-16 mb-4 rounded-full" />
-                <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
+                <h1 className="text-3xl md:text-4xl lg:text-5xl mb-4">
                     Forge Your Knowledge, Play Your Way!
                 </h1>
                 <p className="text-lg mb-8 opacity-80">
@@ -21,18 +21,18 @@ const HeroComponent = () => {
                             Create Quiz
                         </Button>
                     </Link> */}
-                    <Link href="/feature-lesson/create" title="Choose a learning path, create lesson">
-                        <Button className="text-lg mt-4 hover:text-black dark:hover:text-white dark:hover:bg-neutral-900">
+                    <Link href="/feature-lesson/create" title="Create Lesson">
+                        <Button className="text-lg mt-4 dark:hover:bg-neutral-900">
                             Create Lesson
                         </Button>
                     </Link>
                     <Link href="/feature-category-hierarchy/create" title="Structured Learning Path Editor">
-                        <Button className="text-lg mt-4 hover:text-black dark:hover:text-white dark:hover:bg-neutral-900">
+                        <Button className="text-lg mt-4 dark:hover:bg-neutral-900">
                             Create Learning Path
                         </Button>
                     </Link>
                     <Link href="/feature-quiz/mcqs/play" title="Play Javascript quizzes">
-                        <Button className="text-lg mt-4 hover:text-black dark:hover:text-white dark:hover:bg-neutral-900">
+                        <Button className="text-lg mt-4 dark:hover:bg-neutral-900">
                             Play JavaScript Quizzes
                         </Button>
                     </Link>
