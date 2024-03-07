a36837101e95b3c03b8aa9a829210ddc6c5891bd
Sanjib Poon <sanjibpoon123@gmail.com>
2024-03-07 11:13:54 +0545

    Remove shuffleArray

--- app/feature-quiz/mcqs/play/components/Quiz.tsx
+++ app/feature-quiz/mcqs/play/components/Quiz.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useState, useCallback, useEffect } from 'react';
+import { useState } from 'react';
 import Code from './Code';
-import { cn, shuffleArray } from '@/lib/utils';
+import { cn } from '@/lib/utils';
 import {
     Accordion,
     AccordionContent,
@@ -29,12 +29,10 @@ interface optionType {
 export default function Quiz({ quiz }: QuizProps) {
     const [message, setMessage] = useState<string>("");
 
-    const shuffledOptions = shuffleArray(quiz.options);
-
     // Handler for when a choice is selected
-    const handleChoiceSelection = useCallback((option: optionType) => {
+    const handleChoiceSelection = (option: optionType) => {
         setMessage(option.correct ? "Correct" : 'Try Again!');
-    }, []);
+    };
 
 
     return (
@@ -44,12 +42,12 @@ export default function Quiz({ quiz }: QuizProps) {
                 <Code code={quiz.code} />
             </div>
             <ul>
-                {shuffledOptions.map((choice: optionType, index) => (
+                {quiz.options.map((choice: optionType, index) => (
                     <li key={index} className='flex justify-center'>
                         <div
                             onClick={() => handleChoiceSelection(choice)}
                             className={cn(
-                                " p-2 md:p-4 m-2 md:m-4 bg-emerald-100 dark:bg-neutral-800 rounded-xl md:w-80 transition duration-200 ease-in-out transform hover:scale-105 active:scale-95 cursor-pointer",
+                                " p-2 md:p-4 m-2 md:m-4 bg-emerald-200 dark:bg-neutral-800 rounded-xl md:w-80 transition duration-200 ease-in-out transform hover:scale-105 active:scale-95 cursor-pointer",
                             )}
                         >
                             {choice.value}
@@ -57,7 +55,7 @@ export default function Quiz({ quiz }: QuizProps) {
                     </li>
                 ))}
             </ul>
-            {message && <p className='mt-4'>{message}</p>}
+            <p className={cn("mt-4", message ? "opacity-100" : " opacity-100")}>{message}</p>
             <Accordion type="single" collapsible className='w-full'>
                 <AccordionItem value="explanation">
                     <AccordionTrigger>
--- app/feature-quiz/mcqs/play/page.tsx
+++ app/feature-quiz/mcqs/play/page.tsx
@@ -7,8 +7,8 @@ import PlayMcq from './components/PlayMCQ';
 export default async function Page() {
     // const quizzes = await fetchMcqs();
     return (
-        <div className='flex justify-center py-4 bg-cyan-50 dark:bg-cyan-900/30 min-h-screen'>
-            <div className="bg-teal-50 dark:bg-teal-800/50 rounded-3xl shadow-2xl dark:shadow-neutral-800 overflow-auto w-full max-w-xl min-h-[94vh]">
+        <div className='flex justify-center py-4 min-h-screen'>
+            <div className="bg-teal-50 dark:bg-cyan-300/10 rounded-3xl shadow-2xl dark:shadow-none overflow-auto w-full max-w-xl min-h-[94vh]">
                 <PlayMcq mcqs={mcqs} />
             </div>
         </div>
