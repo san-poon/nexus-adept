d1b33dd75e696ffecbcf094b87fc1860c197ab4c
Sanjib Poon <sanjibpoon123@gmail.com>
2024-03-09 13:10:40 +0545

    Minor updates

--- app/components/Card.tsx
+++
@@ -1,7 +0,0 @@
-export default function Card({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="dark:bg-gray-800 p-4 rounded-md shadow-lg">
-            {children}
-        </div>
-    );
-}
