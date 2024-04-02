a788b8a5ef03ab8030f3e49455b80ee65d90d2ec
Sanjib Poon <sanjibpoon123@gmail.com>
2024-04-02 20:29:44 +0545

    Update to use wash colors only for backgrounds, not borders

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "w-full border border-neutral-300 dark:border-neutral-600 rounded-2xl dark:bg-wash-800 placeholder:text-neutral-500 disabled:cursor-not-allowed disabled:opacity-50 dark:placeholder:text-neutral-400 focus-visible:outline-wash-150 dark:focus-visible:outline-none dark:focus-visible:outline-1 dark:focus-visible:outline-offset-0 dark:focus-visible:outline-wash-600",
+          "w-full border border-neutral-300 dark:border-neutral-600 rounded-2xl dark:bg-wash-800 placeholder:text-neutral-500 disabled:cursor-not-allowed disabled:opacity-50 dark:placeholder:text-neutral-400 focus-visible:outline-neutral-400 dark:focus-visible:outline-none dark:focus-visible:outline-1 dark:focus-visible:outline-offset-0 dark:focus-visible:outline-neutral-500",
           className
         )}
         ref={ref}
