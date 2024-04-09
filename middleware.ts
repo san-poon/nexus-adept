6c9870d398e0fe45326fa7f4ee296dc541ec80b8
Sanjib Poon <sanjibpoon123@gmail.com>
2024-04-09 11:24:25 +0545

    Add minimal functional supabase auth

--- middleware.ts
+++ middleware.ts
@@ -1,43 +1,19 @@
-import { auth } from "@/auth"
+import { type NextRequest } from 'next/server';
+import { updateSession } from '@/utils/supabase/middleware';
 
-import {
-    DEFAULT_LOGIN_REDIRECT,
-    apiAuthPrefix,
-    authRoutes,
-    publicRoutes
-} from '@/lib/routes';
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
+}
 
-export default auth((req) => {
-    const { nextUrl } = req;
-    const isLoggedIn = !!req.auth;
-
-    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-
-    // The order of these if clause is important here...
-    if (isApiAuthRoute) {
-        return;
-    }
-    if (isAuthRoute) {
-        if (isLoggedIn) {
-            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-        }
-        return;
-    }
-    if (!isLoggedIn && !isPublicRoute) {
-        return Response.redirect(new URL("/auth/login", nextUrl));
-    }
-    return;
-});
-
-// Optionally, don't invoke Middleware on some paths
 export const config = {
-    matcher: [
-        // Exclude files with a "." followed by an extension, which are typically static files.
-        // Exclude files in the _next directory, which are Next.js internals.
-        "/((?!.+\\.[\\w]+$|_next).*)",
-        // Re-include any files in the api or trpc folders that might have an extension
-        "/(api|trpc)(.*)"
-    ],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
 };
--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,5 +1,5 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/server'
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { NextResponse, type NextRequest } from 'next/server';
 
 export async function updateSession(request: NextRequest) {
     let response = NextResponse.next({
