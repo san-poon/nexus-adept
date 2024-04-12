bd7625f142076cb018f85616bdb6224af4676f69
Sanjib Poon <sanjibpoon123@gmail.com>
2024-04-12 16:41:23 +0545

    Update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { ThemeProvider } from './components/theme-provider';
 import { rubik } from '@/app/fonts';
 
 export const metadata: Metadata = {
-  title: 'Autodidact: Play, Learn, Forge',
-  description: 'Expand your knowledge and unleash your creativity with our comprehesive learning resources and collaborative platform. Start your journey today!',
+  title: 'Nexus Adept: Collaborate & Master, Together!',
+  description: 'Hone your skills with curated learning path designed through collaboration or simply become the collaborator, a master.',
 };
 
 export default function RootLayout({
