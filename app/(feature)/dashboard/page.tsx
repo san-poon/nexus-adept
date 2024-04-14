eefc5c3b553f6bf16ddf1e27a99adcb1289a28b5
Sanjib Poon <sanjibpoon123@gmail.com>
2024-04-14 21:47:19 +0545

    Minor changes

--- app/(feature)/dashboard/page.tsx
+++ app/(feature)/dashboard/page.tsx
@@ -1,16 +1,9 @@
-import { getProfileDTO } from "@/data-access/dto";
 import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
-    const user = await getProfileDTO();
-
-    if (!user) {
-        // This statement can also execute if the logged-in user has no internet connection
-        redirect('/auth/login');
-    }
     return (
-        <section className="h-screen my-4">
-            <p>You are logged in with email: {user.email} confirmed at: {user.emailConfirmedAt}</p>
+        <section>
+            <p>This is a dashboard page.</p>
         </section>
     )
 }
