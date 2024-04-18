be275bc7613f81c3c67cd36ae711f9510b77ff05
Sanjib Poon <sanjibpoon123@gmail.com>
2024-04-18 17:35:36 +0545

    Style changes

--- app/(necessities)/auth/components/SocialForm.tsx
+++ app/(necessities)/auth/components/SocialForm.tsx
@@ -25,14 +25,15 @@ export default function SocialForm() {
                     Sign In with &nbsp; <GithubIcon />
                 </Button>
             </form>
-            {/* <form action={signinWithEmailAction} className="space-y-6">
+            <hr className="dark:border-neutral-600" />
+            <form action={signinWithEmailAction} className="space-y-6">
                 <Input type="email" name="email" placeholder="Email" />
                 <SubmitButton label="Sign In with Email" />
                 {state.message && <p>{state?.message}</p>}
                 <p aria-live="polite" className="sr-only" role="status">
                     {state?.message}
                 </p>
-            </form> */}
+            </form>
         </section>
     );
 }
